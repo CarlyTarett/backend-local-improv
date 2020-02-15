ccc509cf6a49d865d76e91964f0ebac1f50614cd
Carly Tarett <carlytarett@hotmail.com>
2020-02-15 15:40:41 +0000

    get all and post working via postman

--- handler.js
+++ handler.js
@@ -1,18 +1,123 @@
-'use strict';
+const express = require("express");
+const cors = require("cors");
+const serverlessHttp = require("serverless-http");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "improv"
+});
+
+// GET /tasks
+
+app.get("/suggestion", function (request, response) {
+
+    connection.query("SELECT * FROM Suggestions", function (err, data) {
+        if (err) {
+            response.status(500).json({
+                error: err
+            })
+        }
+        else {
+            response.status(200).json({
+                suggestions: data
+            });
+        };
+    });
+
+});
+
+
+// POST /developers
+
+app.post("/suggestion", function (request, response) {
+
+    const newSuggestion = request.body;
+
+    console.log("XXXXXXXXXXXXdoing A CREATE XXXXXXXXXXXxx");
+    connection.query("INSERT INTO Suggestions SET ?", [newSuggestion], function (err, data) {
+
+        if (err) {
+            response.status(500).json({
+                error:
+                    err
+            })
+        }
+        else {
+            newSuggestion.id = data.insertId;
+            response.status(201).json(newSuggestion);
+        }
+    });
+
+});
+
+// PUT /developers
+app.put("/tasks/:id", function (request, response) {
+
+    const updatedTask = request.body;
+    const id = request.params.id;
+
+    console.log("XXXXXXXXXXXXXupdateXXXXXXXXXXXXxx");
+    console.log("id is :   ");
+    console.log(id);
+    console.log(updatedTask);
+
+    console.log("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy");
+
+    connection.query("UPDATE task SET ? WHERE id=?", [updatedTask, id], function (err, data) {
+
+        if (err) {
+            response.status(500).json({
+                error: err
+            })
+        }
+        else {
+            response.status(200).json({
+                developers: data
+            });
+        };
+    });
+
+});
+
+// DELETE /developers
+
+app.delete("/tasks/:id", function (request, response) {
+
+    const id = request.params.id;
+
+
+    console.log("XXXXXXXXXXXXXdeleteXXXXXXXXXXXxx");
+    console.log("id is :   ");
+    console.log(id);
+
+
+    console.log("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy");
+
+
+    //connection.query("Delete FROM task WHERE id = 36", function(err, data){
+
+    connection.query("Delete FROM task WHERE id = ?", [id], function (err, data) {
+        //connection.query("INSERT INTO comments (comment) values ( ?)", [id], function(err, data){
+
+        if (err) {
+            response.status(500).json({
+                error: err
+            })
+        }
+        else {
+            response.sendStatus(200);
+        };
+
+    });
+
+});
+
+module.exports.app = serverlessHttp(app);
